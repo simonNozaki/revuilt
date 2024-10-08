c0279be15dea315c460ff95be20dcaae41280d79
simonNozaki <tldk1993@gmail.com>
2024-10-09 08:48:29 +0900

    test: add Cli specs (#4)
    * wip: add Cli specs
    
    * wip: add Cli specs
    
    * test: add deeply nested directory traversing case
    
    * chore: add coderabbit.yml

--- lib/revuilt/cli.rb
+++ lib/revuilt/cli.rb
@@ -9,16 +9,12 @@ module Revuilt
     class Cli
       include Loggable
 
-      attr_reader :argv,
-                  :dir,
+      attr_reader :dir,
                   :filter_name,
                   :function_symbol,
                   :only_write_temporary
 
-      def initialize(argv)
-        @argv = argv
-        options = RevuiltOptionParser.parse_or_raise(argv)
-
+      def initialize(options)
         @dir = options[:dir]
         @filter_name = options[:filter_name]
         @function_symbol = options[:function_symbol]
@@ -31,53 +27,45 @@ module Revuilt
       end
 
       def swap_file_deep(dir)
-        path_likes = Dir.entries(dir).reject { %w[. ..].include?(_1) }
-        path_likes.each do |path_like|
-          swap_file(dir, path_like)
+        Dir.entries(dir).reject { %w[. ..].include?(_1) }.each do |path_like|
+          entry = "#{dir}/#{path_like}"
+          entry_stat = File.stat(entry)
+
+          if entry_stat.file? && entry.match?(/.vue$/)
+            convert_lines(entry)
+            next
+          end
+
+          next unless entry_stat.directory?
+
+          # Traverse sub directory
+          swap_file_deep(entry)
         end
       end
 
-      def swap_file(dir, path_like)
-        entry = "#{dir}/#{path_like}"
-        entry_stat = File.stat(entry)
+      # read lines and replace to function when there are some matches
+      def convert_lines(entry)
+        lines = File.readlines entry
+        result = FilterConverter.new(lines, filter_name, function_symbol).convert!
+        return false unless result.converted
 
-        if entry_stat.file? && entry.match?(/.vue$/)
-          convert_lines(entry)
-          return
-        end
-
-        return unless entry_stat.directory?
-
-        # Traverse sub directory
-        swap_file_deep(entry)
-      end
-
-      def readlines_or_raise(path)
-        File.readlines(path)
+        logger.info "Entry #{entry} has been converted to function call with #{result.converted_lines.length} lines."
+        replace_to_new_file(result.lines, entry)
+        true
       rescue Errno::ENOENT
         logger.error "Error occurred when reading lines of #{path}."
         raise
       end
 
-      # read lines and replace to function when there are some matches
-      def convert_lines(entry)
-        lines = readlines_or_raise(entry)
-        result = FilterConverter.new(lines, filter_name, function_symbol).convert!
-        return unless result.converted
-
-        logger.info "Entry #{entry} has been converted to function call with #{result.converted_lines.length} lines."
-        replace_to_new_file(result.lines, entry)
-      end
-
       # Write results to temporary file and swap it with original one
-      # TODO: swap new file with old one
       def replace_to_new_file(lines, path)
         tmp_file_path = "#{path}.tmp"
-        File.delete(tmp_file_path) if File.exist?(tmp_file_path)
+        File.delete tmp_file_path if File.exist? tmp_file_path
         File.open(tmp_file_path, 'w') { |file| file.write(lines.join) }
 
-        # TODO: delete original file(add else-clause)
         return if only_write_temporary
+
+        File.rename tmp_file_path, path
       end
     end
 
@@ -85,7 +73,9 @@ module Revuilt
       include Loggable
 
       def replace!(argv)
-        cli = Cli.new(argv)
+        options = RevuiltOptionParser.parse_or_raise argv
+        cli = Cli.new options
+
         logger.info 'Start replacing Vue filter syntax...'
         cli.call
         logger.info 'Complete replacing Vue filter syntax.'
---
+++ spec/revuilt/cli_spec.rb
@@ -0,0 +1,122 @@
+# frozen_string_literal: true
+
+RSpec.describe Revuilt::CLI::Cli do
+  let(:dir) { '' }
+  let(:filter_name) { 'date' }
+  let(:function_symbol) { '$date' }
+  let(:only_write_temporary) { false }
+  let(:options) { { dir:, filter_name:, function_symbol:, only_write_temporary: } }
+  let(:cli) { described_class.new options }
+  let(:cli_spy) { instance_spy 'Cli' }
+
+  # rubocop:disable Metric/BlockLength
+  describe '#swap_file_deep' do
+    context 'current directory has no Vue files' do
+      let(:dir) { '/home/auto-test/demo/' }
+
+      before do
+        stat_double = instance_double File::Stat, file?: true, directory?: false
+        allow(Dir).to receive(:entries).with(dir).and_return %w[. .. Gemfile main.rb]
+        allow(File).to receive(:stat).and_return stat_double
+        allow(cli).to receive(:convert_lines).with('/home/auto-test/demo/Gemfile').and_return false
+      end
+
+      it 'should do nothing' do
+        cli.swap_file_deep dir
+
+        expect(cli_spy).not_to have_received(:convert_lines).with('/home/auto-test/demo/main.rb')
+        expect(cli_spy).not_to have_received(:convert_lines).with('/home/auto-test/demo/Gemfile')
+      end
+    end
+
+    context 'when several Vue files has no Vue filter syntaxes' do
+      let(:dir) { '/home/auto-test/demo/components' }
+
+      before do
+        allow(Dir).to receive(:entries)
+          .with('/home/auto-test/demo/components')
+          .and_return %w[. .. Cart.vue CartItem.vue CartItemPayment.vue]
+        %w[Cart.vue CartItem.vue CartItemPayment.vue].each do |entry|
+          path = "/home/auto-test/demo/components/#{entry}"
+          allow(File).to receive(:stat).with(path).and_return instance_double File::Stat, file?: true, directory?: false
+          allow(cli).to receive(:convert_lines).with(path).and_return false
+        end
+      end
+
+      it 'should not convert anything' do
+        cli.swap_file_deep dir
+
+        expect(cli).to have_received(:convert_lines).with '/home/auto-test/demo/components/Cart.vue'
+        expect(cli).to have_received(:convert_lines).with '/home/auto-test/demo/components/CartItem.vue'
+        expect(cli).to have_received(:convert_lines).with '/home/auto-test/demo/components/CartItemPayment.vue'
+      end
+    end
+
+    context 'when a directory has nested directory and Vue files' do
+      let(:dir) { '/home/auto-test/components' }
+
+      before do
+        allow(Dir).to receive(:entries).with('/home/auto-test/components').and_return %w[. .. README.md Cart.vue cart]
+        '/home/auto-test/components/Cart.vue'.then do |entry|
+          allow(File).to receive(:stat).with(entry).and_return instance_double File::Stat, file?: true,
+                                                                                           directory?: false
+          allow(cli).to receive(:convert_lines).with(entry).and_return true
+        end
+        allow(File).to receive(:stat)
+          .with('/home/auto-test/components/README.md')
+          .and_return instance_double File::Stat, file?: true, directory?: false
+        '/home/auto-test/components/cart'.then do |entry|
+          allow(File).to receive(:stat).with(entry).and_return instance_double File::Stat, file?: false,
+                                                                                           directory?: true
+          allow(Dir).to receive(:entries).with(entry).and_return %w[. .. CartItem.vue CartItemDetail.vue]
+          %w[CartItem.vue CartItemDetail.vue].each do |vue|
+            path = "#{entry}/#{vue}"
+            allow(File).to receive(:stat).with(path).and_return instance_double File::Stat, file?: true, directory?: false
+            allow(cli).to receive(:convert_lines).with(path).and_return false
+          end
+        end
+      end
+
+      it 'should check whether can convert recursively' do
+        cli.swap_file_deep dir
+
+        expect(cli).not_to have_received(:convert_lines).with '/home/auto-test/components/README.md'
+        expect(cli).to have_received(:convert_lines).with '/home/auto-test/components/Cart.vue'
+        expect(cli).to have_received(:convert_lines).with '/home/auto-test/components/cart/CartItem.vue'
+        expect(cli).to have_received(:convert_lines).with '/home/auto-test/components/cart/CartItemDetail.vue'
+      end
+    end
+
+    context 'when directories have multiple nested directories' do
+      let(:dir) { '/home/auto-test/components' }
+
+      before do
+        '/home/auto-test/components'.then do |entry|
+          allow(Dir).to receive(:entries).with(entry).and_return %w[. .. cart]
+        end
+        '/home/auto-test/components/cart'.then do |entry|
+          allow(File).to receive(:stat).with(entry).and_return instance_double File::Stat, file?: false,
+                                                                                           directory?: true
+          allow(Dir).to receive(:entries).with(entry).and_return %w[. .. internals]
+        end
+        '/home/auto-test/components/cart/internals'.then do |entry|
+          allow(File).to receive(:stat).with(entry).and_return instance_double File::Stat, file?: false,
+                                                                                           directory?: true
+          allow(Dir).to receive(:entries).with(entry).and_return %w[. .. CartItemPrice.vue]
+        end
+        '/home/auto-test/components/cart/internals/CartItemPrice.vue'.then do |entry|
+          allow(File).to receive(:stat).with(entry).and_return instance_double File::Stat, file?: true,
+                                                                                           directory?: false
+          allow(cli).to receive(:convert_lines).with(entry).and_return false
+        end
+      end
+
+      it 'should traverse and convert' do
+        cli.swap_file_deep dir
+
+        expect(cli).to have_received(:convert_lines).with '/home/auto-test/components/cart/internals/CartItemPrice.vue'
+      end
+    end
+  end
+  # rubocop:enable Metric/BlockLength
+end
